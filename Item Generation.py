b5e63089077af5f43c1bb9f613725888a1357d85
BunDoData <87618584+dinoskos@users.noreply.github.com>
2022-12-02 00:42:34 -0500

    Update Item Generation.py

--- Item Generation.py
+++ Item Generation.py
@@ -54,9 +54,70 @@ pairs_10 = [["i", "b"], ["i", "e"], ["k", "a"], ["l", "a"], ["l", "c"],
            ["o", "f"], ["o", "k"], ["o", "l"], ["p", "l"], ["q", "i"], 
            ["r", "l"], ["s", "h"], ["s", "k"], ["s", "l"], ["v",  "l"],
            ["w", "b"], ["w", "d"], ["w", "l"], ["w", "o"], ["x", "b"], 
-           ["x", "d"], ["x", "f"], ["x", "l"], ["x", "o"], ["x", "p"]
+           ["x", "d"], ["x", "f"], ["x", "l"], ["x", "o"], ["x", "p"],
            ["y", "l"], ["z", "d"], ["z", "l"]]
-
+pairs_20 = [["f", "c"], ["i", "c"], ["j", "a"], ["j", "b"], ["j", "e"],
+           ["k", "e"], ["k", "f"], ["k", "i"], ["k", "j"], ["l", "g"],
+           ["m", "b"], ["m", "k"], ["q", "k"], ["q", "m"], ["t", "a"],
+           ["t", "e"], ["t", "o"], ["t", "q"], ["u", "f"], ["u", "l"],
+           ["v", "a"], ["w", "e"], ["w", "f"], ["w", "g"], ["w", "h"],
+           ["w", "i"], ["w", "j"], ["w", "k"], ["w", "t"], ["x", "g"],
+           ["x", "h"], ["x", "i"], ["x", "j"], ["x", "n"], ["z", "b"],
+           ["z", "f"], ["z", "g"], ["z", "o"], ["z", "q"]]
+pairs_30 = [["f", "b"], ["h", "a"], ["i", "a"], ["i", "d"], ["i", "h"],
+           ["k", "c"], ["m", "d"], ["m", "f"], ["p", "f"], ["q", "f"], 
+           ["q", "j"], ["q", "l"], ["v", "i"], ["v", "p"], ["w", "q"],
+           ["w", "r"], ["x", "q"], ["x", "t"], ["y", "f"], ["y", "i"],
+           ["z", "h"], ["z", "i"], ["z", "j"]]
+pairs_40 = [["h", "g"], ["i", "f"], ["i", "g"], ["k", "g"], ["n", "f"],
+            ["o", "i"], ["p", "i"], ["p", "m"], ["r", "k"], ["s", "f"],
+            ["s", "i"], ["s", "j"], ["s", "q"], ["t", "m"], ["t", "n"],
+            ["t", "s"], ["u", "k"], ["u", "t"], ["v", "b"], ["v", "d"],
+            ["v", "e"], ["v", "f"], ["v", "g"], ["v", "m"], ["v", "q"],
+            ["w", "a"], ["x", "a"], ["x", "w"], ["y", "a"], ["y", "t"],
+            ["z", "k"]]
+pairs_50 = [["f", "a"], ["f", "d"], ["h", "e"], ["h", "f"], ["l", "h"],
+            ["m", "e"], ["m", "i"], ["n", "d"], ["n", "g"], ["n", "i"],
+            ["o", "h"], ["o", "j"], ["p", "k"], ["r", "i"], ["s", "b"],
+            ["s", "d"], ["s", "n"], ["t", "c"], ["t", "g"], ["t", "h"],
+            ["t", "p"], ["u", "i"], ["u", "p"], ["u", "s"], ["v", "s"],
+            ["w", "p"], ["x", "c"], ["x", "e"], ["y", "m"], ["y", "o"],
+            ["y", "x"], ["z", "p"], ["z", "t"]]
+pairs_60 = [["f", "e"], ["j", "c"], ["j", "d"], ["j", "h"], ["k", "d"],
+            ["l", "b"], ["l", "d"], ["l", "k"], ["m", "c"], ["m", "g"],
+            ["n", "b"], ["n", "j"], ["p", "n"], ["q", "h"], ["q", "n"],
+            ["r", "g"], ["r", "q"], ["s", "m"], ["s", "o"], ["s", "r"],
+            ["t", "k"], ["u", "b"], ["u", "g"], ["u", "q"], ["v", "k"],
+            ["v", "t"], ["w", "c"], ["x", "m"], ["x", "s"], ["x", "u"],
+            ["y", "r"], ["z", "n"], ["z", "v"], ["z", "y"]]
+pairs_70 = [["d", "a"], ["d", "c"], ["e", "d"], ["g", "f"], ["l", "j"],
+            ["m", "a"], ["m", "h"], ["n", "a"], ["n", "e"], ["o", "m"],
+            ["p", "j"], ["q", "e"], ["r", "b"], ["r", "o"], ["s", "g"], 
+            ["t", "b"], ["t", "d"], ["t", "i"], ["t", "r"], ["u", "d"],
+            ["u", "m"], ["v", "r"], ["x", "r"], ["y", "b"], ["y", "c"],
+            ["y", "d"], ["y", "j"], ["y", "k"], ["y", "n"], ["z", "m"], 
+            ["z", "u"]]
+pairs_80 = [["b", "a"], ["c","a"], ["h", "c"], ["k", "b"], ["l", "f"],
+            ["o", "g"], ["o", "n"], ["p", "a"], ["p", "c"], ["p", "h"],
+            ["q", "a"], ["q", "c"], ["q", "o"], ["r", "a"], ["r", "d"],
+            ["r", "f"], ["r", "j"], ["s", "a"], ["s", "p"], ["u", "a"], 
+            ["u", "j"], ["v", "h"], ["v", "j"], ["v", "n"], ["w", "n"],
+            ["w", "s"], ["y", "h"], ["y", "p"], ["y", "w"], ["z", "a"], 
+            ["z", "c"], ["z", "x"]]
+pairs_90 = [["c", "b"], ["e", "b"], ["e", "c"], ["g", "a"], ["g", "b"], 
+            ["g", "c"], ["g", "e"], ["h", "d"], ["j", "f"], ["j", "g"],
+            ["o", "a"], ["o", "b"], ["o", "d"], ["p", "e"], ["r", "c"], 
+            ["r", "e"], ["r", "m"], ["r", "n"], ["r", "p"], ["s", "c"],
+            ["s", "e"], ["t", "j"], ["t", "l"], ["u", "e"], ["u", "h"], 
+            ["u", "o"], ["u", "r"], ["v", "c"], ["v", "o"], ["x", "v"],
+            ["y", "q"], ["z", "e"], ["z", "w"]]
+pairs_100 = [["d", "b"], ["e", "a"], ["g", "d"], ["h", "b"], ["j", "i"],
+            ["k", "h"], ["l", "i"], ["n", "c"], ["n", "h"], ["n", "m"],
+            ["o", "c"], ["o", "e"], ["p", "b"], ["p", "d"], ["p", "g"], 
+            ["p", "o"], ["q", "b"], ["q", "d"], ["q", "g"], ["q", "p"],
+            ["r", "h"], ["t", "f"], ["u", "c"], ["u", "n"], ["v", "u"],
+            ["w", "m"], ["w", "u"], ["w", "v"], ["x", "k"], ["y", "g"], 
+            ["y", "u"], ["y", "v"], ["z", "s"]]
 
 # In[36]:
 
@@ -65,65 +126,87 @@ import string
 import random
 
 
-## Create random generator with beginning placement 
-result = []
-
+## Creating random generator for beginning replacement (first + second placement)
 def id_generator_beginning(size = 5, pairs = pairs_10, chars = string.ascii_lowercase):
-        for pair in pairs:
+    result = []
+    for pair in pairs:
+        placement = random.randint(0, 1)
+        if placement == 0:
             pre_generator = ''.join(random.choice(chars) for _ in range(size - 1))
             pair_list = []
             for letter in pair:
                 pair_list.append(letter + pre_generator)
-            result.append(pair_list)
-        return result
-        
-id_generator_beginning()
+        if placement == 1:
+            pre_generator = ''.join(random.choice(chars) for _ in range(size - 2))
+            prefix = random.choice(chars)
+            pair_list = []
+            for letter in pair:
+                pair_list.append(prefix + letter + pre_generator)    
+        result.append(pair_list)
+    return result
 
+## Note ##
+# size = length of words
+# pairs = choose list of pairs to be replaced
+# num_pairs = number of pairs chosen from list to be replaced
+df = id_generator_beginning(size = 9, pairs = pairs_100)
 
-# In[37]:
-
-
-## Create random generator with end placement 
-result = []
-
+## Creating random generator for end replacement
 def id_generator_end(size = 5, pairs = pairs_10, chars = string.ascii_lowercase):
+    result = []
     for pair in pairs:
-        pre_generator = ''.join(random.choice(chars) for _ in range(size - 1))
-        pair_list = []
-        for letter in pair:
-            pair_list.append(pre_generator + letter)
-        result.append(pair_list)
+        placement = random.randint(0, 1)
+        if placement  == 0:
+            pre_generator = ''.join(random.choice(chars) for _ in range(size - 1))
+            pair_list = []
+            for letter in pair:
+                pair_list.append(pre_generator + letter)
+            result.append(pair_list)
+        if placement == 1:
+            pre_generator = ''.join(random.choice(chars) for _ in range(size - 2))
+            pair_list = []    
+            postfix = random.choice(chars)
+            for letter in pair:
+                pair_list.append(pre_generator + letter + postfix)
+            result.append(pair_list)
     return result
-        
-id_generator_end()
 
+df = id_generator_end(size = 9, pairs = pairs_100)
 
-# In[39]:
-
-
-## Create random generator with middle placement 
-result = []
-
+## Creating random generator for middle replacement
 def id_generator_middle(size = 5, pairs = pairs_10, chars = string.ascii_lowercase):
+    result = []
     for pair in pairs:
         pre_generator = ''.join(random.choice(chars) for _ in range(size - 1))
         pair_list = []
-        for letter in pair:
-            output = pre_generator[:2] + letter + pre_generator[2:]
-            pair_list.append(output)
+        if size == 5:
+            placement = 0  
+            for letter in pair:
+              output = pre_generator[:(2+placement)] + letter + pre_generator[(2+placement):]
+              pair_list.append(output)
+        if size == 7:
+            placement = random.randint(-1, 1)  
+            for letter in pair:
+              output = pre_generator[:(3+placement)] + letter + pre_generator[(3+placement):]
+              pair_list.append(output)
+        if size == 9:
+            placement = random.randint(-2, 2)  
+            for letter in pair:
+              output = pre_generator[:(4+placement)] + letter + pre_generator[(4+placement):]
+              pair_list.append(output)
         result.append(pair_list)
     return result
-        
-id_generator_middle()
 
-
-# In[41]:
-
-
-# Create random generator 
-result = []
-
-def id_generator(size = 5, iteration = 10, chars = string.ascii_lowercase):
+## Note ##
+# size = length of words
+# pairs = choose list of pairs to be replaced
+# num_pairs = number of pairs chosen from list to be replaced
+# placement = varying the level of middle; 0 = median
+df = id_generator_middle(size = 9, pairs = pairs_100)
+  
+## Creating the same pair - for control group
+def id_generator(size = 5, iteration = 20, chars = string.ascii_lowercase):
+    result = []
     for n in range(1, iteration):
         pair = []
         pre_generator = ''.join(random.choice(chars) for _ in range(size))
@@ -132,12 +215,4 @@ def id_generator(size = 5, iteration = 10, chars = string.ascii_lowercase):
         result.append(pair)
     return result
         
-    
-id_generator(iteration = 20)
-
-
-# In[ ]:
-
-
-## Next step
-
+df = id_generator(size = 9, iteration = 200)
